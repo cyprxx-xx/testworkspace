ae0d480039a90f1f7552322c43e7ed27cb082b33
hans <hans@wurst.de>
2011-02-26 11:19:15 +0100

    changes in slider.js

--- js/slider.js
+++ js/slider.js
@@ -2,7 +2,7 @@ document.observe('dom:loaded', init);
 
 function init() {
 	// Slider init
-	
+	var slide = new Slider('scroller', 'left', 'right', 'linear');
 }
 
 
@@ -15,12 +15,25 @@ function init() {
  * Last-Upadte: 2011-02-25
  * Version: 0.1
  */
-var Slider = Class.Create({
+var Slider = Class.create({
 	initialize: function(scroller, left, right, transition) {
 		this.scroller = scroller;
 		this.left = left;
 		this.right = right;
 		this.transition = transition;
+		
+		this.eventHandler(left, right);
+	},
+	eventHandler:function(left, right) {
+		$(left).observe('click', this.getData);
+		$(right).observe('click', this.getData);
+	},
+	getData:function() {
+		var id = this.id;
+		console.debug(id);
 	}
+	
+	
+	
 });
 
